df39ff064e4b06dccf54749333e439c9da48b903
Amit-1807 <150581997+Amit-1807@users.noreply.github.com>
2025-08-04 13:05:59 +0530

    feat: add hero and UI assets, update styling and Vite config

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,23 @@ import { useState , useEffect} from 'react'
 
 import './App.css'
 
-function App() {
+const App = ()  => {
  
   return (
-    <>
+    <main>
+      <div className='pattern'/>
+      
+      <div className="wrapper">
+         <header>
+            <h1>
+              Find <span className='text-gradient'>Movies</span> You'll Enjoy Without hassle
+            </h1>
+         </header>
+
+         <p>Search</p>
+      </div>
      
-    </>
+    </main>
   )
 }
 
